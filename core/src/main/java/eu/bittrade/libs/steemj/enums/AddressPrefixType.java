b42455df2f7da2e8456c157e777480d3c56e92b0
Rithy <uyrithy.ctg@gmail.com>
2025-05-21 21:59:02 +0700

    refresh 2

--- core/src/main/java/eu/bittrade/libs/steemj/enums/AddressPrefixType.java
+++ core/src/main/java/eu/bittrade/libs/steemj/enums/AddressPrefixType.java
@@ -6,6 +6,7 @@ package eu.bittrade.libs.steemj.enums;
  * @author <a href="http://steemit.com/@dez1337">dez1337</a>
  */
 public enum AddressPrefixType {
+    
     /** The Steem address prefix */
     STM,
     /** The Steem test net address prefix */
